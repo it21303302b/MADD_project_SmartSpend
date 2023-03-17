2bbc0d7dc677b59523754b413e29f8a8f97e06f9
IT21303852 <it21303852@my.sliit.lk>
2023-03-18 01:47:06 +0530

    Account updates

--- app/src/main/java/com/example/smartspend/MainActivity.kt
+++ app/src/main/java/com/example/smartspend/MainActivity.kt
@@ -1,6 +1,8 @@
 package com.example.smartspend
 
+import android.content.Intent
 import android.os.Bundle
+import android.view.View
 import com.google.android.material.bottomnavigation.BottomNavigationView
 import androidx.appcompat.app.AppCompatActivity
 import androidx.navigation.findNavController
@@ -13,6 +15,15 @@ class MainActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityMainBinding
 
+    fun loginActivity(view: View){
+        val intent = Intent(this, LoginActivity::class.java)
+        startActivity(intent)
+    }
+
+    fun registerActivity(view: View){
+        val intent = Intent(this, RegisterActivity::class.java)
+        startActivity(intent)
+    }
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
