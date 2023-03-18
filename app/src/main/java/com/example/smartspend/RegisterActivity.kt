35308c431df92a206e2c9adfdb88f8df38fb20b0
bimsa <it21303302@my.sliit.lk>
2023-03-18 23:16:28 +0530

    10th update(UI update and signup and logi crash fix)

--- app/src/main/java/com/example/smartspend/RegisterActivity.kt
+++ app/src/main/java/com/example/smartspend/RegisterActivity.kt
@@ -1,11 +1,19 @@
 package com.example.smartspend
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.TextView
 
 class RegisterActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_register)
+
+        val TV_alreadyLogin = findViewById<TextView>(R.id.TV_alreadyLogin)
+        TV_alreadyLogin.setOnClickListener{
+            val intent = Intent(this,LoginActivity::class.java)
+            startActivity(intent)
+        }
     }
 }
